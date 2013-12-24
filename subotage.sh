3be78eb397f380afb5f1092f5bcdbf9284d06ad3
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-12-24 12:00:13 +0000

    Synopsis: created an overlap correction function

--- subotage.sh
+++ subotage.sh
@@ -964,7 +964,58 @@ function f_guess_format
 
 function f_correct_overlaps
 {
-	echo "not implemented yet"
+    time_type=$(head -n 1 "$g_ProcTmpFile")
+	num_lines=$(($(wc -l "$g_ProcTmpFile" | cut -d ' ' -f 1) - 1))
+    
+	# now I need to rewrite the univeral file once again
+    case $time_type in
+    "secs")
+		# recreate the secs header
+		echo "secs" > "${g_ProcTmpFile}_2"
+        tail -n +2  "$g_ProcTmpFile" | tr -d '\r' |
+		awk "BEGIN {
+				previous_end = 0;			
+				cntr = 0;
+				line_cnt = 0;
+				
+			 }
+			 {				
+				 lines[cntr,0] = NF;
+				 for (i=1; i<=NF; i++) lines[cntr,i] = \$i;
+
+				 if (cntr == 0) {
+					 if ((lines[1,3]+0) > (lines[0,2]+0)) lines[1,3] = lines[0,2];
+				 }
+				 else {
+					 if ((lines[0,3]+0) > (lines[1,2]+0)) lines[0,3] = lines[1,2];
+				 }
+				 
+				 do {
+					 line_cnt++;
+					 cntr = (cntr + 1) % 2;
+
+					 if ((line_cnt >= 2 || line_cnt == $num_lines) && lines[cntr,0]>0) {
+						 printf \"%s %s %s \", lines[cntr,1], lines[cntr,2], lines[cntr,3];
+						 for (i=4; i<=lines[cntr,0]; i++) printf(\"%s \", lines[cntr,i]);
+						 printf \"\n\"; 
+					 }
+				 } while (line_cnt == $num_lines)
+			}" >> "${g_ProcTmpFile}_2"
+
+		# overwrite the original file
+		mv "${g_ProcTmpFile}_2" "${g_ProcTmpFile}"
+	;;
+    
+    "hms" | "hmsms")
+# it's very unlikely that a file in this format will have any overlapping time stamps
+# for the moment this is to be implemented
+	return
+    ;;
+    
+    *)
+    return
+    ;;
+    esac	
 }
 
 ###############################################################################
@@ -1251,6 +1302,7 @@ if [[ $status -ne 0 ]]; then
     f_print_error "Reading error. Error code: [$status]"
     exit
 else
+	f_correct_overlaps;
     status=$($g_Writer "$g_OutputFile")
     
     if [[ $status -ne 0 ]]; then
@@ -1260,7 +1312,7 @@ else
 fi
     
 # remove the temporary processing file
-#rm -rf "$g_ProcTmpFile"
+rm -rf "$g_ProcTmpFile"
 echo "Done"
 
 ###############################################################################
