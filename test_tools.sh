a8778aebd0d8bfa073e52de6665ff4394ebaa856
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2015-02-23 10:37:36 +0000

    Updated license information

--- test_tools.sh
+++ test_tools.sh
@@ -3,6 +3,36 @@
 # force indendation settings
 # vim: ts=4 shiftwidth=4 expandtab
 
+
+########################################################################
+########################################################################
+########################################################################
+
+#  Copyright (C) 2015 Tomasz Wisniewski aka 
+#       DAGON <tomasz.wisni3wski@gmail.com>
+#
+#  http://github.com/dagon666
+#  http://pcarduino.blogspot.co.ul
+# 
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+########################################################################
+########################################################################
+########################################################################
+
+
 test_tool() {
 	eval "$2" > /tmp/tmp_result 2> /tmp/tmp_err
 
