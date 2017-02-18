806b9d5aac51253d2d0a52824ad8723c9cb8b3b4
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2017-02-18 13:34:15 +0000

    Modify run_unit_tests to be able to execute selected unit_tests only
    This change to run_unit_tests.sh allows to specify a set of selected
    tests as positional arguments to the script to be executed, skipping the
    rest from the suite. If no arguments are provided all tests will be
    discovered and executed.

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -12,9 +12,20 @@ DOCKER_IMAGE="napitester"
 # execute the unit tests
 #
 # shellcheck disable=SC2016
-docker run -v "$PWD"/..:/mnt -w /mnt/tests/unit_tests -it napitester bash -c \
-    'for tc in ./*test.sh; do
-        echo " ** executing [$tc]"
-        kcov --exclude-pattern=shunit,mock ../coverage "$tc" ||
-            exit $?
-    done'
+docker run -v "$PWD"/..:/mnt \
+    -w /mnt/tests/unit_tests \
+    -i napitester bash <<CMD_EOF
+
+    # make an array of unit tests
+    tests=( ./*test.sh )
+
+    # run argv unit tests only if provided
+    [ $# -gt 0 ] && tests=( "$@" )
+
+    for tc in "\${tests[@]}"; do
+        echo " ** executing [\$tc]"
+        kcov --exclude-pattern=shunit,mock \
+            ../coverage "\$tc" ||
+            exit \$?
+    done
+CMD_EOF
