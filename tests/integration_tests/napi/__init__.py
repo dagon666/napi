60ba52e83284a0f31ebcb29d7668fb9c0ddaf952
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2017-05-19 19:48:26 +0100

    Initiate logger for integration tests

---
+++ tests/integration_tests/napi/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import logging
+import sys
+
+def prepareLogging(logLevel):
+    formatter = logging.Formatter('%(asctime)s %(message)s')
+
+    handler = (logging.StreamHandler(sys.stderr))
+    handler.setFormatter(formatter)
+    handler.setLevel(logLevel)
+
+    logger = logging.getLogger()
+    logger.setLevel(logLevel)
+    logger.addHandler(handler)
+
+prepareLogging(logging.DEBUG)
