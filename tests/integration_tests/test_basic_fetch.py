f79698ea60a545c5629c2e35ea6ae5d871281aad
Tomasz Wisniewski <tomasz.wisniewski@youview.com>
2017-05-30 16:05:54 +0100

    fetch test wip

--- tests/integration_tests/test_basic_fetch.py
+++ tests/integration_tests/test_basic_fetch.py
@@ -14,6 +14,19 @@ import napi.testcase
 class BasicFetchTest(napi.testcase.NapiTestCase):
 
     def test_ifObtainsAvailableSubtitlesForSingleFile(self):
+        """
+        Brief:
+
+        This test checks if napi is able to download subtitles for a single
+        media files provided directly in the command line.
+
+        Procedure:
+        1. Prepare sandbox and media file
+        2. Call napi with path to media file
+
+        Expected Results:
+        Subtitles file should exist after calling napi.
+        """
         media = None
         with napi.sandbox.Sandbox() as sandbox:
             # generate a media file
@@ -41,6 +54,14 @@ class BasicFetchTest(napi.testcase.NapiTestCase):
             self.assertFalse(fs.xmlExists())
 
     def test_ifObtainsAvailableSubtitlesForMediaInDirectory(self):
+        """
+        Brief:
+
+        Procedure:
+
+        Expected Results:
+
+        """
         mediasAvailable = []
         mediasUnavailable = []
         with napi.sandbox.Sandbox() as sandbox:
@@ -86,6 +107,14 @@ class BasicFetchTest(napi.testcase.NapiTestCase):
                 if napi.fs.Filesystem(m).subtitlesExists()]))
 
     def test_ifDownloadsCoverFilesForSingleMedia(self):
+        """
+        Brief:
+
+        Procedure:
+
+        Expected Results:
+
+        """
         media = None
         with napi.sandbox.Sandbox() as sandbox:
             # obtain an media
@@ -129,6 +158,14 @@ class BasicFetchTest(napi.testcase.NapiTestCase):
             self.assertFalse(fs.xmlExists())
 
     def test_ifDownloadsNfoFilesForSingleMedia(self):
+        """
+        Brief:
+
+        Procedure:
+
+        Expected Results:
+
+        """
         media = None
         with napi.sandbox.Sandbox() as sandbox:
             # obtain media file
@@ -168,6 +205,103 @@ class BasicFetchTest(napi.testcase.NapiTestCase):
 
             self.assertTrue(fs.nfoExists())
             self.assertFalse(fs.xmlExists())
+
+    def test_ifSkipsDownloadingIfSubtitlesAlreadyDownloaded(self):
+        """
+        Brief:
+
+        Procedure:
+
+        Expected Results:
+
+        """
+        pass
+
+# #>TESTSPEC
+# #
+# # Brief:
+# #
+# # Verify if napi works for specified media directory and skips downloading if the subtitles file already exist
+# #
+# # Preconditions:
+# # - napi.sh & subotage.sh must be available in public $PATH
+# # - prepare a set of test files and a test directory structure
+# # - the subtitles files should exist as well
+# #
+# # Procedure:
+# # - Call napi with the path to the pre-prepared media directory
+# #
+# # Expected results:
+# # - napi shouldn't download the subtitles for the media files (for which they are available) if it detects that the
+# # subtitles file already exist
+# #
+# $output = NapiTest::qx_napi($shell, "--stats -s " . $NapiTest::testspace);
+# %output = NapiTest::parse_summary($output);
+# is ($output{skip}, $total_available, "Total number of skipped");
+# is ($output{skip} + $output{unav}, $output{total}, "Total processed (with skipping)");
+# is ($output{total}, $total_available + $total_unavailable, "Total processed (with skipping) 2");
+# 
+
+    def test_ifSkipsFilesSmallerThanConfiguredLimit(self):
+        """
+        Brief:
+
+        Procedure:
+
+        Expected Results:
+
+        """
+        pass
+# #>TESTSPEC
+# #
+# # Brief:
+# #
+# # Verify if napi works for specified media directory and skips the files smaller than specified (-b option)
+# #
+# # Preconditions:
+# # - napi.sh & subotage.sh must be available in public $PATH
+# # - prepare a set of test files and a test directory structure
+# #
+# # Procedure:
+# # - Call napi with the path to the pre-prepared media directory
+# # - specify various values for the -b option
+# #
+# # Expected results:
+# # - napi shouldn't download the subtitles for the media files (for which they are available) which are smaller than specified
+# #
+# NapiTest::clean_testspace();
+# prepare_assets();
+# 
+# # prepare big files
+# my $dir_cnt = 0;
+# foreach my $dir (glob ($NapiTest::testspace . '/*')) {
+# 
+# 	my $basepath = $dir . "/test_file";
+# 
+# 	$basepath =~ s/([\<\>\'\"\$\[\]\@\ \&\#\(\)]){1}/\\$1/g;
+# 	# print 'After: ' . $basepath . "\n";
+# 
+# 	system("dd if=/dev/urandom of=" . $basepath .  $_ . ".avi bs=1M count=" . $_)
+# 		foreach(15, 20);
+# 	$dir_cnt++;
+# }
+# 
+# $output = NapiTest::qx_napi($shell, "--stats -b 12 " . $NapiTest::testspace);
+# %output = NapiTest::parse_summary($output);
+# is ($output{unav}, $dir_cnt * 2, "Number of processed files bigger than given size");
+# 
+# $output = NapiTest::qx_napi($shell, "--stats -b 16 " . $NapiTest::testspace);
+# %output = NapiTest::parse_summary($output);
+# is ($output{unav}, $dir_cnt, "Number of processed files bigger than given size 2");
+# 
+# $output = NapiTest::qx_napi($shell, "--stats -b 4 " . $NapiTest::testspace);
+# %output = NapiTest::parse_summary($output);
+# is ($output{total},
+# 	$output{unav} + $output{ok},
+# 	"Number of processed files bigger than given size 2");
+# 
+# NapiTest::clean_testspace();
+# done_testing();
 
 if __name__ == '__main__':
     napi.testcase.runTests()
