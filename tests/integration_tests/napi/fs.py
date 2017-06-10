8d8ad20896e29098c2a96d56e0a1ffab58849b8b
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2017-06-10 14:40:59 +0100

    Add support for custom extension in fs module for integration tests

--- tests/integration_tests/napi/fs.py
+++ tests/integration_tests/napi/fs.py
@@ -9,7 +9,11 @@ class Filesystem(object):
     """
     def __init__(self, media):
         self.media = media
-        self.path = os.path.dirname(self.media['path'])
+        self.path = self._dirname()
+        self.noExtension = self._basename()
+
+    def _dirname(self):
+        return os.path.dirname(self.media['path'])
 
     def _basename(self):
         return os.path.splitext(self.media['name'])[0]
@@ -18,29 +22,27 @@ class Filesystem(object):
         path = os.path.join(self.path, fileName)
         return os.path.exists(path) and os.path.getsize(path) > 0
 
-    def createSubtitlesFileNames(self, abbreviation = None):
-        extensions = [ 'srt', 'sub', 'txt' ]
-        noExt = self._basename()
-        if abbreviation:
-            noExit = '.'.join((noExt, abbreviation))
+    def createSubtitlesFileNames(self, abbreviation = None, extension = None):
+        extensions = set([ extension ] if extension else [ 'srt', 'sub', 'txt' ])
+        noExt = ('.'.join((self.noExtension, abbreviation)) if
+                abbreviation else self.noExtension)
+
         return map(lambda ext: noExt + '.' + ext, extensions)
 
     def createNfoFileName(self):
-        noExt = self._basename()
-        return noExt + '.nfo'
+        return self.noExtension + '.nfo'
 
     def createCoverFileName(self):
-        noExt = self._basename()
-        return noExt + '.jpg'
+        return self.noExtension + '.jpg'
 
     def createXmlFileName(self):
-        noExt = self._basename()
-        return noExt + '.xml'
+        return self.noExtension + '.xml'
 
-    def subtitlesExists(self, abbreviation = None):
+    def subtitlesExists(self, abbreviation = None, extension = None):
         paths = [ os.path.exists(p) for p in map(
             lambda f: os.path.join(self.path, f),
-            self.createSubtitlesFileNames(abbreviation)) ]
+            self.createSubtitlesFileNames(abbreviation, extension)) ]
+
         return any(paths)
 
     def coverExists(self):
